ed8ac74d54aeb62ac47bfc53283fa3b76694109f
darke7 <darke7@163.com>
2018-04-08 03:47:23 +0800

    get recommend data

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,15 +1,38 @@
 <template>
   <div>
-      recomend
+      <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
+        
+      </div>
   </div>
 </template>
 
 <script>
+import {ERR_OK} from 'api/config'
+import {getRecommend} from 'api/recommend'
 export default {
   data () {
     return {
+    	recommends: [],
     }
+  },
+  mounted(){
+  	this._getRecommend();
+  },
+  methods:{
+  	_getRecommend(){
+  		getRecommend().then((res) => {
+			if (res.code === ERR_OK) {
+				this.recommends = res.data.slider;
+				console.log(this.recommends);
+			}
+        })
+  	},
+
+  },
+  components:{
+
   }
+
 }
 </script>
 
